4384e9330a0e1e8a8e4fe3dc04a7e6d35e03d48f
nojaf <florian.verdonck@outlook.com>
2023-06-25 00:25:23 +0200

    Update local url.

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -32,5 +32,5 @@ export const ordersCollections = collection(store, "orders");
 
 export const apiUrl =
   window.location.hostname === "localhost"
-    ? "http://127.0.0.1:5001/ten-miles-elverdinge-2023/us-central1/createPaymentLink"
+    ? "http://127.0.0.1:5001/ten-miles-elverdinge-2023/europe-west1/createPaymentLink"
     : "https://createpaymentlink-otquim2qwa-ew.a.run.app";
