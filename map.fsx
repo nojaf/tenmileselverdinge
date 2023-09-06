c6423df497e018cccabb64c9ff73c88c4ae05238
nojaf <florian.verdonck@outlook.com>
2023-09-06 09:56:27 +0200

    Export to gpx

--- map.fsx
+++ map.fsx
@@ -4,14 +4,15 @@
 open System.IO
 open Suave
 open Thoth.Json.Net
+open System.Xml.Linq
 
-let lngLatDecoder =
+let lngLatDecoder: string -> JsonValue -> Result<(float * float),DecoderError> =
     Decode.object (fun get ->
         let lng = get.Required.Field "lng" Decode.float
         let lat = get.Required.Field "lat" Decode.float
         lng, lat)
 
-let lgnLatEncoder lng lat =
+let lgnLatEncoder (lng: float) (lat: float): JsonValue =
     Encode.object [ "lng", Encode.float lng; "lat", Encode.float lat ]
 
 let coords =
@@ -44,7 +45,7 @@ let handler (ctx: HttpContext) =
             return! Successful.ACCEPTED $"Added %A{newCoords}" ctx
     }
 
-startWebServer defaultConfig handler
+// startWebServer defaultConfig handler
 
 #r "nuget: CoordinateSharp, 2.19.1.1"
 
@@ -69,3 +70,36 @@ let totalDistance (file: FileInfo) =
 totalDistance (FileInfo(Path.Combine(__SOURCE_DIRECTORY__, "src", "components", "8km-trail.json")))
 
 totalDistance (FileInfo(Path.Combine(__SOURCE_DIRECTORY__, "src", "components", "ten-miles-trail.json")))
+
+
+
+let (!-) p = FileInfo p
+
+let mkGpxFile (input:FileInfo) =
+    let output = Path.ChangeExtension(input.FullName, ".gpx")
+    let coordinates =
+        let json = File.ReadAllText input.FullName
+        match Decode.fromString (Decode.array lngLatDecoder) json with
+        | Result.Error err-> failwith err
+        | Result.Ok coordinates -> coordinates
+
+    let gpx =
+        XDocument(
+            XElement(XName.Get("gpx", "http://www.topografix.com/GPX/1/1"),
+                XAttribute(XName.Get("version"), "1.1"),
+                XElement(XName.Get("trk"),
+                    XElement(XName.Get("trkseg"),
+                        Seq.map (fun (lng,lat) ->
+                            XElement(XName.Get("trkpt"),
+                                XAttribute(XName.Get("lat"), string lat),
+                                XAttribute(XName.Get("lon"), string lng)
+                            )
+                        ) coordinates
+                    )
+                )
+            )
+        )
+    gpx.Save(output)
+
+mkGpxFile !- @"C:\Users\nojaf\Projects\tenmileselverdinge\src\components\8km-trail.json"
+mkGpxFile !- @"C:\Users\nojaf\Projects\tenmileselverdinge\src\components\ten-miles-trail.json"
